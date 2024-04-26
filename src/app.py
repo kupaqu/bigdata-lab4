9b93801dc344129cbaf944feaacbe55c10885cd9
Buda Vampilov <fccfcbyjhtk@gmail.com>
2024-04-27 00:36:12 +0900

    edited port in app.py

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ logger = Logger(SHOW_LOG)
 log = logger.get_logger(__name__)
 
 # hbase connection
-client = HBaseRESTClient(['http://0.0.0.0:8081'])
+client = HBaseRESTClient(['http://0.0.0.0:8080'])
 admin = HBaseAdmin(client)
 put = Put(client)
 get = Get(client)
